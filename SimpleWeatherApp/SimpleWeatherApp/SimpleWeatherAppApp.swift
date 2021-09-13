cf659e7d8e472bc1f61fb9cbfb4b041a2a372c48
Bogdan Alex Ciobanu <82522105+alexciobanu808@users.noreply.github.com>
2021-09-13 11:47:19 -0400

    Initialized project

---
+++ SimpleWeatherApp/SimpleWeatherApp/SimpleWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWeatherAppApp.swift
+//  SimpleWeatherApp
+//
+//  Created by Alex Ciobanu on 9/13/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
