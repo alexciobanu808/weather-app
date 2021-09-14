3fa11fd8b992b34c19e7df82ddc35d09d3c7ca95
Bogdan Alex Ciobanu <82522105+alexciobanu808@users.noreply.github.com>
2021-09-14 19:58:35 -0400

    Refactor to use camel case

--- SimpleWeatherApp/SimpleWeatherApp/Networking/Models/CurrentWeatherForecast.swift
+++ SimpleWeatherApp/SimpleWeatherApp/Networking/Models/CurrentWeatherForecast.swift
@@ -21,10 +21,18 @@ struct CurrentWeatherForecast: Decodable {
     
     struct TemperatureDetails: Decodable {
         let temp: Double
-        let feels_like: Double //TO-DO make camel case
-        let temp_min: Double
-        let temp_max: Double
+        let feelsLike: Double
+        let tempMin: Double
+        let tempMax: Double
         let humidity: Int
+        
+        enum CodingKeys: String, CodingKey {
+            case temp
+            case feelsLike = "feels_like"
+            case tempMin = "temp_min"
+            case tempMax = "temp_max"
+            case humidity
+        }
     }
     
     struct WindDetails: Decodable {
